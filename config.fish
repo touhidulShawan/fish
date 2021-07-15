74fad8600db48b5e31420117d58043cfc8e19cd0
Touhidul Shawan <touhidulshawan@gmail.com>
2021-07-15 11:02:51 +0600

    "

--- config.fish
+++ config.fish
@@ -82,15 +82,15 @@ alias pscpu='ps auxf | sort -nr -k 3'
 alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
 
 # youtube-dl
-alias yta-aac="youtube-dl --extract-audio --audio-format aac "
-alias yta-best="youtube-dl --extract-audio --audio-format best "
-alias yta-flac="youtube-dl --extract-audio --audio-format flac "
-alias yta-m4a="youtube-dl --extract-audio --audio-format m4a "
-alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
-alias yta-opus="youtube-dl --extract-audio --audio-format opus "
-alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis "
-alias yta-wav="youtube-dl --extract-audio --audio-format wav "
-alias ytv-best="youtube-dl -f bestvideo+bestaudio "
+alias yta-aac="youtube-dl --extract-audio --audio-format aac"
+alias yta-best="youtube-dl --extract-audio --audio-format best"
+alias yta-flac="youtube-dl --extract-audio --audio-format flac"
+alias yta-m4a="youtube-dl --extract-audio --audio-format m4a"
+alias yta-mp3="youtube-dl --extract-audio --audio-format mp3"
+alias yta-opus="youtube-dl --extract-audio --audio-format opus"
+alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis"
+alias yta-wav="youtube-dl --extract-audio --audio-format wav"
+alias ytv-best="youtube-dl -f bestvideo+bestaudio"
 
 #alias for copy
 alias cp='cp -r -g -i'
